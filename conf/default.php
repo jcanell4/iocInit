ffa22a36845852ae450360b2e2c9508120dbb0e6
jcanell4 <jcanell4@ioc.cat>
2014-05-20 23:22:52 +0200

    Changing to 1.9.3 version of dojo library

--- conf/default.php
+++ conf/default.php
@@ -5,8 +5,8 @@
 
 // Jquery's url from jQuery
 $conf['jquery_url'] = 'http://code.jquery.com/jquery.min.js';
-$conf['dojo_url'] = "//ajax.googleapis.com/ajax/libs/dojo/1.8/dojo/dojo.js";
-$conf['dojo_theme_base'] = "//ajax.googleapis.com/ajax/libs/dojo/1.8/dijit/themes/";
+$conf['dojo_url'] = "//ajax.googleapis.com/ajax/libs/dojo/1.9.3/dojo/dojo.js";
+$conf['dojo_theme_base'] = "//ajax.googleapis.com/ajax/libs/dojo/1.9.3/dijit/themes/";
 $conf['dojo_theme'] = "claro";
 $conf['ns_manager_grp'] = "nsmanager";
 $conf['sectokParamName'] = "sectok";
