6ace023f881410ac63cff4477c31f38873756efe
jcanell4 <jcanell4@xtec.cat>
2017-05-06 01:26:21 +0200

    reverted the dojo version. At the moment version 1.12 fails.

--- conf/default.php
+++ conf/default.php
@@ -5,8 +5,10 @@
 
 // Jquery's url from jQuery
 $conf['jquery_url'] = 'http://code.jquery.com/jquery.min.js';
+//$conf['dojo_theme_base'] = "//ajax.googleapis.com/ajax/libs/dojo/1.12.1/";
 $conf['dojo_url_base'] = "//ajax.googleapis.com/ajax/libs/dojo/1.10.4/";
 $conf['dojo_url'] = $conf['dojo_url_base']."dojo/dojo.js";
+//$conf['dojo_theme_base'] = "//ajax.googleapis.com/ajax/libs/dojo/1.12.1/dijit/themes/";
 $conf['dojo_theme_base'] = "//ajax.googleapis.com/ajax/libs/dojo/1.10.4/dijit/themes/";
 $conf['dojo_theme'] = "claro";
 $conf['ns_manager_grp'] = "nsmanager";
