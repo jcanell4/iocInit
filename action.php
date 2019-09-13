847f83a2506f766cec18e920d649e535cd94296e
rclaver <rclaver@xtec.cat>
2019-09-13 10:52:52 +0200

    normalización parámetro de DokuWiki_Action_Plugin: Doku_Event_Handler $controller

--- action.php
+++ action.php
@@ -2,17 +2,15 @@
 /**
  * @author     Josep Cañellas <jcanell4@ioc.cat>
  */
-
-// must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
-
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 require_once(DOKU_PLUGIN.'ownInit/init.php');
 require_once(DOKU_INC.'inc/common.php');
 
 class action_plugin_ownInit extends DokuWiki_Action_Plugin {
-    function register(&$controller) {
+
+    function register(Doku_Event_Handler $controller) {
         $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'init_ownInit');
         $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'init_header_output');
     }
@@ -20,41 +18,41 @@ class action_plugin_ownInit extends DokuWiki_Action_Plugin {
     function init_ownInit(&$event, $param) {
         own_init($this->getPluginName());
     }
-    
+
     function init_header_output(&$event, $param) {
         $this->loaddojo($event);
         //$this->loadjquery($event);
     }
-    
-    
+
+
     private function loadjquery(&$event) {
         //TO DO
     }
-    
+
     private function loaddojo(&$event) {
         global $conf;
         global $INFO;
         if(isset($INFO["isDojoLoaded"])&& $INFO["isDojoLoaded"]){
             return;
         }
-        
+
         $INFO["isDojoLoaded"]=true;
         $event->data["link"][] = array ("rel" => "stylesheet",
                                     "href" => $this->getConf('dojo_theme_base')
                                               .$this->getConf('dojo_theme')
                                               .'/'.$this->getConf('dojo_theme')
-                                              .'.css', 
+                                              .'.css',
                                     "media" => "screen");
         $item0 = $event->data["script"][0];
         $item1 = $event->data["script"][1];
-        
-        
+
+
         $event->data["script"][0] = array (
                                         "type" => "text/javascript",
                                         "charset" => "utf-8",
                                         "_data" => "",
                                         "src" =>  $this->getConf('dojo_url'),
-        );        
+        );
 
         $event->data["script"][1] = array (
                 "type" => "text/javascript",
@@ -69,8 +67,8 @@ class action_plugin_ownInit extends DokuWiki_Action_Plugin {
                                            $this->getConf('dojo_theme')."\");\n".
                             "});\n",
         );
-        
-        array_unshift($event->data["script"], array( 
+
+        array_unshift($event->data["script"], array(
                 "type" => "text/javascript",
                 "charset" => "utf-8",
                 "_data" => "var dojoConfig = {\n".
@@ -78,16 +76,16 @@ class action_plugin_ownInit extends DokuWiki_Action_Plugin {
                         "   async:true,\n".
                         "   baseUrl: '/iocjslib/',\n".
                         "   tlmSiblingOfDojo: false,\n".
-                        "   locale: \"".hsc($conf["lang"])."\",\n". 
+                        "   locale: \"".hsc($conf["lang"])."\",\n".
                         "   packages: [\n".
                         "       {\"name\":\"ioc\",\"location\":\"/iocjslib/ioc\"},\n".
                         "       {\"name\":\"dojo\",\"location\":\"".$this->getConf('dojo_url_base')."dojo\"},\n".
                         "       {\"name\":\"dijit\",\"location\":\"".$this->getConf('dojo_url_base')."dijit\"},\n".
                         "       {\"name\":\"dojox\",\"location\":\"".$this->getConf('dojo_url_base')."dojox\"}\n".
                         "   ]\n".
-                        "};\n",));             
+                        "};\n",));
         array_unshift($event->data["script"], $item1);
         array_unshift($event->data["script"], $item0);
     }
-}        
+}
 
